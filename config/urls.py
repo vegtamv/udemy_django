790952a7a115ebbb08e2b81a0092e885aa92b1ed
vegtam <jjch8030@naver.com>
2024-05-03 18:41:48 +0900

    8-4 첫 번째 Django 애플리케이션

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # app의 urls.py 연결 위해 import
 
 urlpatterns = [
-    path('my_app/', include('my_app.urls')),
+    path('my_app/', include('my_app.urls')), # my_app의 urls.py 연결
     path('admin/', admin.site.urls),
 ]
