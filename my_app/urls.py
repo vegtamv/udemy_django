37cc17f7b6a605644d84d854d0566b12f227a6c2
vegtam <jjch8030@naver.com>
2024-06-08 19:48:21 +0900

    9-2 동적 뷰-라우팅 로직 실습

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index") #/my_apps --> Project urls.py
+    #path('', views.index, name="index"), #/my_apps --> Project urls.py,
+    path('<topic>', views.news_view), # /my_apps/topic
+    path('<int:num1>/<int:num2>', views.add_view) # /my_apps/topic
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello This is a View Inside My_App")
+articles = {
+    'sports' : 'Sports Page',
+    'finance' : 'Finance Page',
+    'politics' : 'PoIitics Page'
+}
+
+# def index(request):
+#     return HttpResponse("Hello This is a View Inside My_App")
+
+def news_view(request, topic):
+    return HttpResponse(articles[topic])
+
+def add_view(request, num1, num2):
+    # domain.com/my_app/num1/num2 --> num1+num2
+    result = num1 + num2
+    return HttpResponse(str(result))
