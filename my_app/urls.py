e456163bc2667ef12e9d142d4dd2534284d2c99d
vegtam <jjch8030@naver.com>
2024-06-16 00:32:34 +0900

    9-6 템플릿에 뷰 연결하기

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name="index"), #/my_apps --> Project urls.py,
-    path('<int:num_page>', views.num_page_view), # /my_apps/num_page
-    path('<str:topic>', views.news_view, name="topic-page"), # /my_apps/topic   
-    #path('<int:num1>/<int:num2>', views.add_view) # /my_apps/topic
+    path('',views.simple_view) # domain.com/my_app
 ]
+
+# urlpatterns = [
+    #path('', views.index, name="index"), #/my_apps --> Project urls.py,
+    # path('<int:num_page>', views.num_page_view), # /my_apps/num_page
+    # path('<str:topic>', views.news_view, name="topic-page"), # /my_apps/topic   
+    #path('<int:num1>/<int:num2>', views.add_view) # /my_apps/topic
+# ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,36 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404,  HttpResponseRedirect
 from django.urls import reverse
 
-articles = {
-    'sports' : 'Sports Page',
-    'finance' : 'Finance Page',
-    'politics' : 'PoIitics Page'
-}
+def simple_view(request):
+    return render(request,'my_app/example.html')
 
-# def index(request):
-#     return HttpResponse("Hello This is a View Inside My_App")
+# articles = {
+#     'sports' : 'Sports Page',
+#     'finance' : 'Finance Page',
+#     'politics' : 'PoIitics Page'
+# }
 
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        result = "Topic Not Found"
-        #return HttpResponseNotFound(result)
-        raise Http404("404 Generic Error") # 추후 404.html에 연결
+# # def index(request):
+# #     return HttpResponse("Hello This is a View Inside My_App")
 
-def add_view(request, num1, num2):
-    # domain.com/my_app/num1/num2 --> num1+num2
-    result = num1 + num2
-    return HttpResponse(str(result))
+# def news_view(request, topic):
+#     try:
+#         result = articles[topic]
+#         return HttpResponse(articles[topic])
+#     except:
+#         result = "Topic Not Found"
+#         #return HttpResponseNotFound(result)
+#         raise Http404("404 Generic Error") # 추후 404.html에 연결
 
-# Redirect
-# domain/com/my_app/0 --> domain/com/my_app/sports
+# def add_view(request, num1, num2):
+#     # domain.com/my_app/num1/num2 --> num1+num2
+#     result = num1 + num2
+#     return HttpResponse(str(result))
 
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
-    topic = topics_list[num_page]
+# # Redirect
+# # domain/com/my_app/0 --> domain/com/my_app/sports
 
-    # webpage = reverse('topic-page', args=[topic]) # 첫번째 인자는 urls.py에 정의한 name
-    # return HttpResponseRedirect(webpage)
-    return HttpResponseRedirect(reverse('topic-page', args=[topic]))
+# def num_page_view(request, num_page):
+#     topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
+#     topic = topics_list[num_page]
+
+#     # webpage = reverse('topic-page', args=[topic]) # 첫번째 인자는 urls.py에 정의한 name
+#     # return HttpResponseRedirect(webpage)
+#     return HttpResponseRedirect(reverse('topic-page', args=[topic]))
