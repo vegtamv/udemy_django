eb05b0d764b5719a48a945e8491c925d46bf6582
vegtam <jjch8030@naver.com>
2024-07-16 18:18:04 +0900

    11-2 데이터 상호작용: 생성, 조회

--- my_app/models.py
+++ my_app/models.py
@@ -5,3 +5,6 @@ class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
+
+    def __str__(self):
+        return  f"{self.last_name}, {self.first_name} is {self.age} years old."
