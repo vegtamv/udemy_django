d85c5f94049dc43af8582ac816e0a2e9d9eb26bd
vegtam <jjch8030@naver.com>
2024-08-03 05:23:45 +0900

    12-2 모델과 웹사이트-2 (cars)

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 
 # Create your views here.
 def list(request):
-    return render(request, 'cars/list.html')
+    all_cars = models.Car.objects.all()
+    context = {'all_cars':all_cars}
+
+    return render(request, 'cars/list.html', context=context)
 
 def add(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand, year=year)
+        # 유저가 새 Car를 등록했다면 list.html로 이동
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+        if request.POST:
+            pk = request.POST['pk'] 
+            try:
+                models.Car.objects.get(pk=pk).delete()
+                return redirect(reverse('cars:list'))
+            except:
+                print('pk not found')
+                return redirect(reverse('cars:list'))
+        else:
+            return render(request, 'cars/delete.html')
