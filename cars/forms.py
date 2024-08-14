b19a419c7dd5caab456687e4573b0bd4a12f1f47
vegtam <jjch8030@naver.com>
2024-08-14 20:28:45 +0900

    13-3 위젯과 스타일링

--- cars/forms.py
+++ cars/forms.py
@@ -4,4 +4,10 @@ class ReviewForm(forms.Form):
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
     email = forms.EmailField(label="Email", required=False)
-    review = forms.CharField(label="리뷰를 작성해주세요.", max_length=100, required=False)
+    review = forms.CharField(label="리뷰를 작성해주세요.", max_length=100, 
+                             required=False,
+                             widget=forms.Textarea(attrs={
+                                 'class':'myform',
+                                 'rows':'2',
+                                 'cols':'6'})
+                             )
