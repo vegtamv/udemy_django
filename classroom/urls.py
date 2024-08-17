87235fd65e262c0f1190c5ac299cf8a69cfa9440
vegtam <jjch8030@naver.com>
2024-08-17 19:05:13 +0900

    14-2 FormView

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 #from .views import home_view
-from .views import HomeView, ThankuView
+from .views import HomeView, ThankuView, ContactFormView
 
 app_name = 'classroom'
 
 urlpatterns = [
     #path('',home_view,name='home')
     path('',HomeView.as_view(),name='home'),
-    path('thanku/',ThankuView.as_view(),name='thanku')
+    path('thanku/',ThankuView.as_view(),name='thanku'),
+    path('contact/',ContactFormView.as_view(),name='contact')
 ]
--- classroom/views.py
+++ classroom/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
+from .forms import ContactForm
 
 # Create your views here.
 # def home_view(request):
@@ -10,3 +12,17 @@ class HomeView(TemplateView):
 
 class ThankuView(TemplateView):
     template_name = "classroom/thanku.html" 
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = "classroom/contact.html" 
+
+    # Success URL (Not HTML file)
+    # success_url = "/classroom/thanku/" 
+    success_url = reverse_lazy('classroom:thanku')
+
+    # What to do with form
+    def form_valid(self, form):
+        #form.save()
+        print(form.cleaned_data)
+        return super().form_valid(form)
